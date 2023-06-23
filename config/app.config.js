ce17bba59085cfef15a8806755a7cacfb503127e
JENNA EBOT <jennaagbor013@gmail.com>
2023-06-23 13:53:50 +0100

    done

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config;
 
 const MONGO_DB_CONFIG ={
-    DB: 'mongodb+srv://jennaagbor013:tX67RrGaHnWxvXT6@cluster0.lnuwkyj.mongodb.net/?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://jennaagbor013:tX67RrGaHnWxvXT6@cluster0.lnuwkyj.mongodb.net/ReactBackendendProject?retryWrites=true&w=majority'
 }
 
 module.exports ={
